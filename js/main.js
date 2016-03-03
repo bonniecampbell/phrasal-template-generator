9e71eccadb0b5e2c61b34c0b347748e6fef8c6ee
bonniecampbell <bonnie.jp.campbell@hotmail.com>
2016-03-03 13:53:27 -0500

    fixed indentation

--- js/main.js
+++ js/main.js
@@ -12,11 +12,11 @@ verb = prompt('A past tense verb (ran, burped, etc.)');
 story = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning');
 
 if (story == 'a') {
-document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
+  document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
 
 } if (story == 'b') {
   document.write('Ameilia ' + verb + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space ' + creature + '.');
 
 } if (story == 'c') {
-document.write('Jackson chose his ' + adjective + ',' +  colour + creature + 'card and' + verb + ' it to the table knowing he won.');
+  document.write('Jackson chose his ' + adjective + ',' +  colour + creature + 'card and' + verb + ' it to the table knowing he won.');
 }
