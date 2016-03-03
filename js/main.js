7f1586ed6537f84937a8237e884a1d1ac5b65540
bonniecampbell <bonnie.jp.campbell@hotmail.com>
2016-03-03 13:38:03 -0500

    added the 'vars' and story info

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var colour;
+var creature;
+var adjective;
+var story;
+
+alert('Let’s make a story together!');
+
+colour = prompt(A colour (orange, indigo, etc.))
+crearure = prompt(A creature (dragon, moth, etc.))
+adjective = prompt(An adjective (beautiful, super, etc.))
+verb = prompt(A past tense verb (ran, burped, etc.))
+story = prompt(Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning)
+
+if (story == 'a') {
+document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
+
+} if (story == 'b') {
+  document.write('Ameilia ' + verb + ' through the ' + adjective + colour + 'nebula escaping the space' + 'nebula escaping the space');
