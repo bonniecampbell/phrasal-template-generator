c2be92cdd41cb22b570283b63ef0d5d071e09a12
bonniecampbell <bonnie.jp.campbell@hotmail.com>
2016-03-03 13:52:50 -0500

    fixed issues in my spelling and code

--- js/main.js
+++ js/main.js
@@ -5,18 +5,18 @@ var story;
 
 alert('Let’s make a story together!');
 
-colour = prompt(A colour (orange, indigo, etc.))
-crearure = prompt(A creature (dragon, moth, etc.))
-adjective = prompt(An adjective (beautiful, super, etc.))
-verb = prompt(A past tense verb (ran, burped, etc.))
-story = prompt(Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning)
+colour = prompt('A colour (orange, indigo, etc.)');
+creature = prompt('A creature (dragon, moth, etc.)');
+adjective = prompt('An adjective (beautiful, super, etc.)');
+verb = prompt('A past tense verb (ran, burped, etc.)');
+story = prompt('Which story would you like? \n a) Snacking \n b) Escaping \n c) Winning');
 
 if (story == 'a') {
 document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
 
 } if (story == 'b') {
-  document.write('Ameilia ' + verb + ' through the ' + adjective + colour + 'nebula escaping the space' + 'nebula escaping the space');
+  document.write('Ameilia ' + verb + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space ' + creature + '.');
 
-} if (story == 'b') {
-document.write('Jackson chose his ' + adjective + ' , ' +  colour + creature + 'card and' + verb + 'it to the table knowing he won.');
+} if (story == 'c') {
+document.write('Jackson chose his ' + adjective + ',' +  colour + creature + 'card and' + verb + ' it to the table knowing he won.');
 }
